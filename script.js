971ca8497de46bf8b9105ec642d31ca78f8aca17
Elshod <elshod45>
2024-03-16 15:01:26 +0500

    gg

---
+++ script.js
@@ -0,0 +1,45 @@
+document.querySelector("#heading").textContent = "Привет Элшод"
+
+
+console.log("Привет мир!");
+
+console.info("Это вывод информации");
+
+console.warn("это предупреждение");
+
+console.error("произошла ошибка");
+
+console.log(3 + 4 - 7 + 1 + 2 * 3 / 2);
+
+const variable1 = 4
+
+let variable2 = 5
+
+var variable3 = 6
+
+
+console.log(variable1 + variable2);
+
+
+let val1 = document.querySelector("#value1").value
+let val2 = document.querySelector("#value2").value
+
+let output = +val1 + +val2
+
+console.log(output);
+
+document.querySelector("#output").textContent = "приветчик"
+
+console.log("приветчик");
+
+console.info("Это вывод информации");
+
+console.warn("это предупреждение");
+
+console.error("произошла ошибка");
+
+const variable4 = 5
+
+let variable5 = 5
+
+var variable6 = 7
